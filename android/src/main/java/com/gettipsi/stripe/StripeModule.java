ad113f936ed9191acd0d71d73a0ac17a2e0920ea
Muccy <muccymac@gmail.com>
2021-02-05 12:20:20 +0100

    Restoring ArgCheck.notEmptyString which was proper

--- android/src/main/java/com/gettipsi/stripe/StripeModule.java
+++ android/src/main/java/com/gettipsi/stripe/StripeModule.java
@@ -182,7 +182,7 @@ public class StripeModule {
 
   @ReactMethod
   public void setStripeAccount(final String stripeAccount) {
-    // ArgCheck.notEmptyString(mPublicKey); // commented out because when stripeAccount is null, it throws
+    ArgCheck.notEmptyString(mPublicKey);
     if (stripeAccount == null) {
       mStripe = new Stripe(applicationContext, mPublicKey);
     } else {
